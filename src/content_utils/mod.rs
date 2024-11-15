599a3571af1c1ab296e619389d926a6bfdb4b92f
Jaxon <booga956@gmail.com>
2024-11-14 23:39:11 -0700

    nvm

--- src/content_utils/mod.rs
+++ src/content_utils/mod.rs
@@ -34,10 +34,8 @@ pub fn get_regex(content: String) -> String {
     if matches.len() == 0 {
         return "".to_string();
     }
-    match matches.iter().next() {
-        None => "".to_string(),
-        Some(x) => REGEX_MAP[x].to_string(),
-    }
+    // thread 'tokio-runtime-worker' panicked at src/content_utils/mod.rs:38:44: called `Option::unwrap()` on a `None` value
+    return REGEX_MAP[matches.iter().next().unwrap()].to_string();
 }
 pub fn is_valid(content: &str) -> Content {
     if regex::Regex::new(TWITTER_REGEX).unwrap().is_match(content) {
